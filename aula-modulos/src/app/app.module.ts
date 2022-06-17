14f2afcfa7662b6c72137ea99bdccb2f5a6bf17d
Josie Flores <drijosie@gmail.com>
2022-06-17 12:53:08 -0300

    aula de modulos

--- aula-modulos/src/app/app.module.ts
+++ aula-modulos/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { SharedModule } from './shared/shared.module';
+import { SellModule } from './features/sell/sell.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    SellModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
